0a114e5eedc9aafc07911bcc817a24312a0e04c4
IsraelCastillo26 <101540799+IsraelCastillo26@users.noreply.github.com>
2024-11-12 19:06:52 -0500

    Add files via upload

---
+++ Practica 4.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Practica 4</title>
+</head>
+
+<body>
+<?php
+echo "<b> Practica 4 <br> Palma Castillo Israel </b> <br><br>";
+$numero = 7;
+//verificar si el numero es par o impar
+if($numero % 2 == 0){
+    echo "El numero $numero es par.";
+}
+else {
+    echo "El numero $numero es impar.";
+}
+?>
+</body>
+</html>
---
+++ Practica 5.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+	<title>Practica 5</title>
+</head>
+
+<body>
+<?php
+echo "<b> Practica 5 <br> Palma Castillo Israel </b> <br><br>";
+$dia = "Miercoles";
+
+switch($dia){
+    case "Lunes":
+            echo "Hoy es Lunes";
+            break;
+    case "Martes":
+            echo "Hoy es Martes";
+            break;
+    case "Miercoles";
+            echo "Hoy es Miercoles";
+            break;
+    case "Jueves";
+            echo "Hoy es Jueves";
+            break;
+    case "viernes";
+            echo "Hoy es Viernes";
+            break;
+    case "Sabado";
+            echo "Hoy es Sabado";
+            break;
+    case "Domingo";
+            echo "Hoy es Domingo";
+            break;
+    default:        
+}
+?>
+</body>
+</html>
